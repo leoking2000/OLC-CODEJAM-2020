a8240635a2e1003cd22d75d37d2d18ec858c52f8
leoking <Leoking470@gmail.com>
2020-09-01 20:31:23 +0300

    movement done

--- Assets/Scripts/PlayerContoler.cs
+++ Assets/Scripts/PlayerContoler.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class PlayerContoler : MonoBehaviour
 {
 
-    public float moveSpeed = 2;
-    public float jumpForce = 10;
+    public float moveSpeed = 30f;
+    public float jumpForce = 15f;
+    public float yVelLimit = 7f;
+    public float fallMultiplier = 1.2f;
+    public float lowJumpMultiplier = 1.2f;
 
 
     Rigidbody rb;
+
     float horizontalInput;
-    bool  jumpKeyPress;
+    float  jumpKeyPress;
+
+    bool jumpBuffer = true;
 
     State state = State.Grounded;
 
-    private void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
     }
@@ -23,33 +29,68 @@ public class PlayerContoler : MonoBehaviour
     private void Update()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
-        jumpKeyPress = Input.GetKey(KeyCode.UpArrow);
-
+        jumpKeyPress = Input.GetAxisRaw("Jump");
     }
 
     private void FixedUpdate()
     {
-        //transform.Translate(new Vector3(horizontalInput, 0, 0) * moveSpeed * Time.deltaTime);
-
+        // move
         rb.AddForce(new Vector3(horizontalInput, 0, 0) * moveSpeed * Time.deltaTime, ForceMode.VelocityChange);
 
-        if (jumpKeyPress && state == State.Grounded)
+        if (jumpKeyPress > 0 && state == State.Grounded)
         {
-            rb.AddForce(new Vector3(0, jumpForce, 0),ForceMode.VelocityChange);
+
+            if (jumpBuffer)
+            {
+                Jump();
+                jumpBuffer = false;
+            }
+        }
+        else
+        {
+            jumpBuffer = true;
         }
 
 
-
-
-
-        if (rb.velocity.y == 0)
+        if (rb.velocity.y > yVelLimit)
         {
-            state = State.Grounded;
+            rb.velocity = new Vector3(rb.velocity.x, yVelLimit, 0);
         }
+        else if (rb.velocity.y > 0 && jumpKeyPress <= 0)
+        {
+            rb.AddForce(Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1), ForceMode.VelocityChange);
+        }
+        else if (rb.velocity.y < 0)
+        {
+            rb.AddForce(Vector3.up * Physics.gravity.y * (fallMultiplier - 1), ForceMode.VelocityChange);
+        }
+
+
+        // checkpoint
+        if (transform.position.y < -10)
+        {
+            transform.position = new Vector3(-111, -2.5f, 0);
+        }
+
     }
 
 
+    // private
+    private void Jump()
+    {
+        rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.VelocityChange);
+    }
+
+
+
+
+    // Collition
     private void OnCollisionEnter(Collision collision)
+    {
+        state = State.Grounded;
+    }
+
+    private void OnCollisionStay(Collision collision)
     {
         state = State.Grounded;
     }
