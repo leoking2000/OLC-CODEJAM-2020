8266a502b0c00a30db1e71a5d9b5a810feb8f184
leoking <Leoking470@gmail.com>
2020-09-06 16:34:24 +0300

    end sceen

--- Assets/Scripts/EndLevel.cs
+++ Assets/Scripts/EndLevel.cs
@@ -4,14 +4,10 @@ using UnityEngine.SceneManagement;
 public class EndLevel : MonoBehaviour
 {
     public int index;
-    public bool isLastLevel;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(isLastLevel == false)
-        {
-            SceneManager.LoadScene(index);
-        }
+        SceneManager.LoadScene(index);
     }
 
 }
